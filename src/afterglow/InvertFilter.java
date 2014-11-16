85896e1f08e63cbedc806be05e60cf3784a99936
Victor Porras <vporras@ymail.com>
2014-11-16 09:51:51 +0000

    adding invert filter
    Signed-off-by: Victor Porras <vporras@ymail.com>

---
+++ src/afterglow/InvertFilter.java
@@ -0,0 +1,20 @@
+package afterglow;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
+public class InvertFilter extends Filter {
+
+	public InvertFilter(Filter filter) {
+		super(filter);
+	}
+
+	public Mat process(Mat oldFrame, Mat newFrame) {
+		Mat after = new Mat();//newFrame.size(), CvType.CV_64F);
+		Mat invertcolormatrix= new Mat(newFrame.rows(),newFrame.cols(), newFrame.type(), new Scalar(255,255,255));
+		Core.subtract(invertcolormatrix, newFrame, after);
+		return super.process(oldFrame, after);
+	}
+
+}
